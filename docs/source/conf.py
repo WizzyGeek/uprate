0b7d92e2458f98be97d37764bd5c4d9243acbf37
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-07-26 21:16:48 +0530

    [📃] Update doc theme to Furo!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,11 +50,26 @@ if release is None: # fail early
 extensions = [
     "sphinx.ext.autodoc",
     "myst_parser",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon"
 ]
 
 autodoc_member_order = 'bysource'
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
 
 
 # MyST extensions
@@ -78,19 +93,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = "_static/uprate_icon.png"
 
-html_theme_options = {
-    "logo": "uprate_icon.png",
-    "logo_name": True,
-    "logo_text_align": "center"
-}
+# html_theme_options = {
+#     "logo": "uprate_icon.png",
+#     "logo_name": True,
+#     "logo_text_align": "center"
+# }
 
 _up_static = Path(__file__).parent / "_static"
 
